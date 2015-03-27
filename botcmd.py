cd3a7ba01b0c5b15ca5babc940a294b91fca3e8b
Juhani Haverinen <juhani.haverinen@gmail.com>
2015-03-27 17:53:58 +0200

    First commit

---
+++ botcmd.py
@@ -0,0 +1,15 @@
+# Commands on which to call `execcmd`
+concmd = []
+
+# Executed on every message
+# `line` is message split on ' '
+# irc:
+#     irc.send(line)           Send `line` over IRC
+#     irc.msg(chan/nick, msg)  Send `msg` using PRIVMSG to `chan/nick`
+#     irc.nick                 The nick the bot was started with
+#     irc.chan                 The channels the bot was started with
+def parse((line, irc)):
+	return
+
+def execcmd(cmd):
+	return
